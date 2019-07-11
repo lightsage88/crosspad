68f8136b3e6ef04e82ab1b7ed24cb2cadc764a6b
Adrian <adrian.e.rosales@gmail.com>
2019-07-11 03:24:11 -0700

    reput some jargon ahead of requests

--- server/index.js
+++ server/index.js
@@ -1,10 +1,28 @@
 const express = require('express');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 const pino = require('express-pino-logger')();
 
 const app = express();
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(pino);
+app.use(cors());
+
+const corsOptions = {
+    origin: 'https://api-v3.igdb.com'
+}
+
+app.options('*', cors());
+app.post(`${process.env.REACT_APP_IGDB_API_URL}/games/?search=mario&fields=
+name,
+id`, cors(corsOptions), (req, res)=>{
+console.log(req, res);
+})
+
+app.get('/', (req,res)=>{
+    const {url} =  req.query;
+
+})
 
 app.get('/api/greeting', (req, res) => {
   const name = req.query.name || 'World';
--- src/App.js
+++ src/App.js
@@ -6,12 +6,46 @@ import Loader from './components/Loader';
 import './App.css';
 
 class App extends Component {
- 
+
+  state = {
+    name: '',
+    greeting: ''
+  }
+
+
+  
+
 
   render() {
+
     
+   
+    const handleChange = (event)=> {
+      this.setState({ name: event.target.value });
+    }
+  
+    const handleSubmit = (event)=> {
+      event.preventDefault();
+      fetch(`/api/greeting?name=${encodeURIComponent(this.state.name)}`)
+        .then(response => response.json())
+        .then(state => this.setState(state));
+    }
+
+   
+
     return (
       <div className="App">
+                <form onSubmit={handleSubmit}>
+            <label htmlFor="name">Enter your name: </label>
+            <input
+              id="name"
+              type="text"
+              value={this.state.name}
+              onChange={handleChange}
+            />
+            <button type="submit">Submit</button>
+          </form>
+          <p>{this.state.greeting}</p>
       <Loader />
         <Container id="appContainer">
           <Header />
