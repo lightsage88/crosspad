696396781063d475a6eea507a5a10b38826413d8
Adrian <adrian.e.rosales@gmail.com>
2019-07-13 21:19:00 -0700

    going to switch up to react-testing-library, I think

--- src/tests/MainGameData.test.js
+++ src/tests/MainGameData.test.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import MainGameData from '../components/MainGameData';
 import {shallow, mount} from 'enzyme';
 import { exportAllDeclaration } from '@babel/types';
 
-it('renders without crashing', () => {
-    const wrapper = shallow(<MainGameData />);
-    expect(wrapper.length).toEqual(1);
-});
+describe('<MainGameData />', ()=>{
+    it('renders without crashing', () => {
+        const wrapper = shallow(<MainGameData />);
+        expect(wrapper.length).toEqual(1);
+    });
+
+    // it('will show the Notice component if there is no gameData in state', ()=>{
+
+    // })
+
+})
+
+
 
 
 
--- src/tests/Results.test.js
+++ src/tests/Results.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Provider} from '../components/Context';
 import Results from '../components/Results';
-import {shallow, mount} from 'enzyme';
+import {shallow, mount, render, dive} from 'enzyme';
 
 
 describe('Results', ()=>{
@@ -9,5 +9,20 @@ describe('Results', ()=>{
     it('renders without crashing', ()=>{
         const wrapper = shallow(<Results />);
         expect(wrapper.length).toEqual(1);
+    });
+
+    it('makes a list of buttons if context.searchOptions is populated', ()=>{
+        const context = {
+            searchOptions: ['Mario', 'Zelda','Sonic']
+        };
+
+        const wrapper = render(<Provider value={{searchOptions: ['Mario']}}><Results /></Provider>,);
+        console.log(<Provider />)
+
+        console.log(wrapper);
+        expect(wrapper.find('ul.gameButtonUL').length).toEqual(1);
+        
+        // expect(wrapper.find('li').html()).not.toEqual("Mario");
+        //TODO: will have to cover this with UI testing
     })
 })
